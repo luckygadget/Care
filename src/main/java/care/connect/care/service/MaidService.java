3c76fb2f273b0da910a094fc0ae021433fbe3d84
naveensabavath <naveen.sabavath@gaiansolutions.com>
2025-11-25 16:00:37 +0530

    added maid service

--- src/main/java/care/connect/care/service/MaidService.java
+++ src/main/java/care/connect/care/service/MaidService.java
@@ -1,4 +1,22 @@
 package care.connect.care.service;
 
+import care.connect.care.dto.request.MaidRequestDto;
+import care.connect.care.dto.request.UserRequestDto;
+import care.connect.care.dto.response.MaidResponseDto;
+import care.connect.care.dto.response.UserResponseDto;
+import care.connect.care.exception.ApiException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface MaidService {
+
+    MaidResponseDto createMaid(MaidRequestDto requestDto) throws ApiException;
+
+    MaidResponseDto getMaidById(String id);
+
+    Page<MaidService> getAllMaids(Pageable page);
+
+    MaidResponseDto updateMaidById(String id, MaidRequestDto requestDto);
+
+    void deleteMaid(String id) throws ApiException;
 }
--- src/main/java/care/connect/care/service/impl/MaidServiceImpl.java
+++ src/main/java/care/connect/care/service/impl/MaidServiceImpl.java
@@ -1,4 +1,69 @@
 package care.connect.care.service.impl;
 
-public class MaidServiceImpl {
+import care.connect.care.dto.request.MaidRequestDto;
+import care.connect.care.dto.response.MaidResponseDto;
+import care.connect.care.exception.ApiException;
+import care.connect.care.mapper.MaidMapper;
+import care.connect.care.models.Maid;
+import care.connect.care.repositories.MaidRepository;
+import care.connect.care.service.MaidService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MaidServiceImpl implements MaidService {
+
+    private final MaidRepository maidRepository;
+    private final MaidMapper maidMapper;
+
+
+    @Override
+    public MaidResponseDto createMaid(MaidRequestDto requestDto) throws ApiException {
+        log.info("====> Creating New Maid: :{}", requestDto);
+
+        if(maidRepository.existsByPhoneNumber(requestDto.getPhoneNumber())){
+            throw new ApiException("Maid already Exist");
+        }
+
+        Maid maid = maidMapper.toEntity(requestDto);
+        Maid savedMaid = maidRepository.save(maid);
+        return maidMapper.toResponseDto(savedMaid);
+    }
+
+    @Override
+    public MaidResponseDto getMaidById(String id) {
+
+        log.info("====> Fetching Maid by Id: {}", id);
+        Maid maid = maidRepository.findById(id)
+                .orElseThrow() -> new ApiException("Maid Not Found");
+
+        return maidMapper.toResponseDto(maid);
+
+    }
+
+    @Override
+    public Page<MaidService> getAllMaids(Pageable page) {
+        log.info("Fetching maids: page {} size {}", page.getPageNumber(), page.getPageSize());
+
+        return maidRepository.findAll(pageable).map(maidMapper::toResponseDto);
+
+    }
+
+    @Override
+    public MaidResponseDto updateMaidById(String id, MaidRequestDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteMaid(String id) throws ApiException {
+
+    }
 }
