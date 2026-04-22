b705b5408237adb61eea44bf007bc8516f1bcf69
luckygadget <120310255+luckygadget@users.noreply.github.com>
2026-04-22 16:03:01 +0530

    Update UserRepository.java

--- src/main/java/care/connect/care/repositories/UserRepository.java
+++ src/main/java/care/connect/care/repositories/UserRepository.java
@@ -4,7 +4,12 @@ import care.connect.care.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+    Optional<User> findByEmail(String email);  // ? add this
+
 }
